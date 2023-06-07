e44f57d27dd68f3d9ae2aeb9593e9b27203147f0
Son Do <dominhson580@gmail.com>
2023-06-07 21:58:42 +0700

    Read and parse data from kepler

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+const results = [];
+
+fs.createReadStream("./kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    results.push(data);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(results);
+    console.log("Done!");
+  });
